5bbb3774fcac6d0f2c375014a2107476c3c89e10
bobster12345 <38799868+bobster12345@users.noreply.github.com>
2018-12-05 17:48:28 -0500

    new

---
+++ src/old stuff/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import SignInPage from "./SignInPage";
+import Dashboard from "./Components/Dashboard";
+
+class App extends Component {
+  render() {
+    //return <SignInPage />;
+    return <Dashboard />;
+  }
+}
+
+export default App;
